95ffb10ac93ca191af0ed507d589f1f0141659f0
Yash Malviya <ymalviya@oneshield.com>
2025-12-09 15:31:22 +0530

    day 8

---
+++ day_8_part_2.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class dsu {
+public:
+  vector<int> p, sz;
+  int n;
+  dsu(int _n) : n(_n) {
+    p.resize(n, -1);
+    sz.resize(n, 1);
+    for (int i = 0; i < n; ++i) {
+      p[i] = i;
+    }
+  }
+ 
+  inline int get(int v) {
+    if (v == p[v]) {
+      return v;
+    }
+    return p[v] = get(p[v]);
+  }
+ 
+  inline void unite(int u, int v) {
+    int rootu = get(u);
+    int rootv = get(v);
+    if (rootu != rootv) {
+      if (sz[rootu] < sz[rootv]) {
+        swap(rootu, rootv);
+      }
+      p[rootv] = rootu;
+      sz[rootu] += sz[rootv];
+    }
+  }
+};
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  string s;
+  vector<vector<int>> points; 
+  while (cin >> s) {
+    string p = "";
+    vector<int> cord;
+    for (char c : s) {
+      if (c == ',') {
+        cord.push_back(stoi(p));
+        p.clear();
+      } else {
+        p.push_back(c);
+      }
+    }
+    cord.push_back(stoi(p));
+    points.push_back(cord);
+  }
+  int n = points.size();
+  auto getd = [&](vector<int>& p1, vector<int>& p2) -> double {
+    double dx = double(p1[0]) - double(p2[0]);
+    double dy = double(p1[1]) - double(p2[1]);
+    double dz = double(p1[2]) - double(p2[2]);
+    return sqrt(dx * dx + dy * dy + dz * dz);
+  };
+  map<vector<int>, int> id;
+  map<int, vector<int>> idc;
+  int next = 0;
+  for (auto &v : points) {
+    if (id.count(v) == 0) {
+      idc[next] = v;
+      id[v] = next++;
+    }
+  }
+  dsu dis(next);
+  priority_queue<vector<double>, vector<vector<double>>, greater<vector<double>>> pq;
+  for (int i = 0; i < n - 1; i++) {
+    for (int j = i + 1; j < n; j++) {
+      pq.push({getd(points[i], points[j]), (double) id[points[i]], (double) id[points[j]]});
+    }
+  }
+  long long ans = 0;
+  while (pq.size()) {
+    auto v = pq.top();
+    pq.pop();
+    int id1 = v[1];
+    int id2 = v[2];
+    if (dis.get(id1) != dis.get(id2)) {
+      dis.unite(id1, id2);
+      vector<int> x = idc[id1], y = idc[id2]; 
+      ans = (long long) x[0] * y[0];
+    }
+  }
+  cout << ans;
+  return 0;
+}
